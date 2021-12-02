339083204dbef0533322fc2ea5ee7ba6ec9db19d
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-01 23:43:44 -0600

    Add day 2 part 2 solution

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,12 +1,12 @@
 use std::{fs, ops::Index};
 
-pub fn travel() -> i32 {
+pub fn travel() -> (i32, i32) {
     let data = fs::read_to_string("src/day02/inputs.txt").expect("Unable to read file");
     let split_commands = data
         .split('\n')
         .map(|cmd| cmd.split(' ').collect::<Vec<&str>>());
 
-    let total_moved = split_commands.fold((0, 0), |res, cmd| {
+    let total_moved = split_commands.clone().fold((0, 0), |res, cmd| {
         let delta = cmd.index(1).parse::<i32>().unwrap();
 
         let point = match cmd.index(0) {
@@ -19,5 +19,21 @@ pub fn travel() -> i32 {
         (res.0 + point.0, res.1 + point.1)
     });
 
-    total_moved.0 * total_moved.1
+    let total_moved_with_aim = split_commands.fold((0, 0, 0), |res, cmd| {
+        let delta = cmd.index(1).parse::<i32>().unwrap();
+
+        let point = match cmd.index(0) {
+            &"forward" => (delta, delta * res.2, 0),
+            &"up" => (0, 0, -delta),
+            &"down" => (0, 0, delta),
+            _ => (0, 0, 0),
+        };
+
+        (res.0 + point.0, res.1 + point.1, res.2 + point.2)
+    });
+
+    (
+        total_moved.0 * total_moved.1,
+        total_moved_with_aim.0 * total_moved_with_aim.1,
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day02;
 
 fn main() {
-    let mult_movement = day02::travel();
+    let (mult_movement, mult_movement_aim) = day02::travel();
     println!("the submarine traveled x * y: {}", mult_movement);
+    println!(
+        "the submarine traveled x * y with aim: {}",
+        mult_movement_aim
+    );
 }
