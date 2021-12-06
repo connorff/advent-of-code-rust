0fd7b67b665e5086351d1d799cc8d751bc8a8574
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-06 11:29:43 -0600

    Add day 6 part 2 solution

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::{collections::HashMap, fs};
 
 #[derive(Copy, Clone)]
 struct LanternFish {
@@ -15,27 +15,46 @@ impl LanternFish {
 
         return vec![];
     }
+
+    fn descendants_after_days(&self, days: i32) -> Vec<LanternFish> {
+        let mut fish_vec = vec![self.clone()];
+
+        for _ in 0..days {
+            for x in 0..fish_vec.len() {
+                let fish = &mut fish_vec[x];
+
+                let mut new_fish = fish.add_day();
+                fish_vec.append(&mut new_fish);
+            }
+        }
+
+        fish_vec
+    }
 }
 
-pub fn main() -> usize {
+pub fn main() -> i64 {
     let data = fs::read_to_string("src/day06/fish.txt").expect("Unable to read file");
-    let initial_fish: Vec<i32> = data
+    let initial_fish: Vec<LanternFish> = data
         .split(",")
         .map(|num| num.parse::<i32>().unwrap())
-        .collect();
-    let mut fish_vec: Vec<LanternFish> = initial_fish
-        .into_iter()
         .map(|timer| LanternFish { timer })
         .collect();
 
-    for _ in 0..80 {
-        for x in 0..fish_vec.len() {
-            let fish = &mut fish_vec[x];
+    let fish_map_nums: Vec<i32> = (0..9).collect();
+    let fish_num_map: HashMap<_, _> = fish_map_nums
+        .into_iter()
+        .map(|n| LanternFish { timer: n })
+        .map(|fish| (fish.timer, fish.descendants_after_days(128).len() as i64))
+        .collect();
 
-            let mut new_fish = fish.add_day();
-            fish_vec.append(&mut new_fish);
-        }
-    }
+    let fish_vec: Vec<LanternFish> = initial_fish
+        .into_iter()
+        .map(|fish| fish.descendants_after_days(128))
+        .flatten()
+        .collect();
 
-    fish_vec.len()
+    fish_vec
+        .into_iter()
+        .map(|f| *fish_num_map.get(&f.timer).unwrap())
+        .sum()
 }
