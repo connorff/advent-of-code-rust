e9fd12791d1d539783eb06924055e883ee1861c9
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-09 10:13:12 -0600

    Add day 7 part 2 solution

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,5 +1,9 @@
 use std::fs;
 
+fn get_dst(dst: i32) -> i32 {
+    (dst * (dst + 1)) / 2
+}
+
 pub fn main() -> i32 {
     let data = fs::read_to_string("src/day07/crabs.txt").expect("Unable to read file");
     let mut crab_positions: Vec<i32> = data
@@ -15,5 +19,15 @@ pub fn main() -> i32 {
         crab_positions[mid - 1]
     };
 
-    crab_positions.iter().fold(0, |total, pos| total + (pos - median).abs())
+    let mut res = i32::MAX;
+    for num in *crab_positions.first().unwrap()..*crab_positions.last().unwrap() {
+        let curr_res = crab_positions
+            .iter()
+            .fold(0, |total, pos| total + get_dst((pos - num).abs()));
+        if curr_res < res {
+            res = curr_res;
+        }
+    }
+
+    res
 }
